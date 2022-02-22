3fa13284a1a1dbeaa4281f8421c83c454f76b459
yeongno <dldudsh83@gmail.com>
2022-02-23 00:12:06 +0900

    get_type,random

---
+++ main.js
@@ -0,0 +1,7 @@
+import _ from 'lodash'
+import getType from './getType'
+import getRandom from './getRandom'
+
+console.log(_.camelCase('the hello world'))
+console.log(getType([1,2,3]))
+console.log(getRandom(),getRandom())
