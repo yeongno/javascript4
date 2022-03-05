744aff45e833905eaad909af7f96c701484358a0
yeongno <dldudsh83@gmail.com>
2022-03-05 21:36:07 +0900

    보내기와 내보내기

---
+++ getRandom.js
@@ -0,0 +1,7 @@
+export function random(){
+  return Math.floor(Math.random()*10)
+}
+export const user ={
+  name: 'HEROPY',
+  age: 85
+}
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import _ from 'lodash'
 import getType from './getType'
-import getRandom from './getRandom'
+// import {random, user as heropy} from './getRandom'
+import * as R from './getRandom'
 
 console.log(_.camelCase('the hello world'))
 console.log(getType([1,2,3]))
-console.log(getRandom(),getRandom())
+// console.log(random(),random())
+console.log(R)
