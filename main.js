0df3158ad5a4a3dbecd5992617e75d5278f6879d
yeongno <dldudsh83@gmail.com>
2022-03-06 21:28:15 +0900

    정규식 생성

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log(123)
+const str = `
+010-1234-5678
+thesecon@gmail.com
+http://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`
+
+// const regexp = new RegExp('the', 'gi')
+//옵션 g는 the라는 문자를 찾는다. gi는 대소문자 가리지 않고 찾는다.
+const regexp =/the/gi
+console.log(str.match(regexp))
